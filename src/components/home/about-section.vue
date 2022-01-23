ed1f509559ad534c9b3e7a0a4b05be685420aaac
redcodemohammed <redcodemohammed@gmail.com>
2022-01-23 22:36:09 +0300

    make the data dynamic

--- src/components/home/about-section.vue
+++ src/components/home/about-section.vue
@@ -1,58 +1,66 @@
 <template>
-	<section id="about">
-		<!-- title -->
-		<h2>00. About</h2>
+    <section id="about">
+        <!-- title -->
+        <h2>00. About</h2>
 
-		<p>
-			Hello, my name is
-			<router-link to="/" class="red">Mohammed</router-link> I enjoy making things with web
-			technologies, I started with the web development back in 2017, when I found out that I
-			want to make websites! <br />
-			<br />
-			Right now, I am working at a start up,
-			<a href="https://turtanu.com" target="_blank" class="blue">Turtanu</a>, where I am
-			focusing on building user interfaces with
-			<a href="https://vuejs.org/" target="_blank" class="red">Vue.js</a> for our new ideas
-			and projects. My main focus these days is focusing on my design skills, trying new techs
-			on both the front-end and the back-end. <br />
-			<br />
-			Here is a list of some technologies I enjoy working with:
-		</p>
+        <p>
+            Hello, my name is
+            <router-link to="/" class="red">Mohammed</router-link> I enjoy making things with web technologies, I started with the web
+            development back in 2017, when I found out that I want to make websites! <br />
+            <br />
+            Right now, I am working at a start up,
+            <a href="https://turtanu.com" target="_blank" class="blue">Turtanu</a>, where I am focusing on building user interfaces with
+            <a href="https://vuejs.org/" target="_blank" class="red">Vue.js</a> for our new ideas and projects. My main focus these days is
+            focusing on my design skills, trying new techs on both the front-end and the back-end. <br />
+            <br />
+            Here is a list of some technologies I enjoy working with:
+        </p>
 
-		<div class="technologies">
-			<div class="technology">javascript</div>
-			<div class="technology">typescript</div>
-			<div class="technology">react</div>
-			<div class="technology">node.js</div>
-			<div class="technology">vue.js</div>
-			<div class="technology">feathers.js</div>
-			<div class="technology">tailwind.css</div>
-			<div class="technology">mongodb</div>
-			<div class="technology">express.js</div>
-		</div>
-	</section>
+        <div class="technologies">
+            <a :href="technology.link" target="_blank" class="technology" v-for="technology in technologies" :key="technology.id">{{
+                technology.name
+            }}</a>
+        </div>
+    </section>
 </template>
-
+<script>
+export default {
+    name: 'AboutSection',
+    data: () => ({
+        technologies: [
+            { id: 0, name: 'Javascript', link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript' },
+            { id: 1, name: 'Typescript', link: 'https://www.typescriptlang.org/' },
+            { id: 2, name: 'React', link: 'https://reactjs.org/' },
+            { id: 3, name: 'Node.js', link: 'https://nodejs.org/en/' },
+            { id: 4, name: 'Vue.js', link: 'https://vuejs.org/' },
+            { id: 5, name: 'Feathers.js', link: 'https://feathersjs.com/' },
+            { id: 6, name: 'Tailwind.css', link: 'https://tailwindcss.com/' },
+            { id: 7, name: 'MongoDB', link: 'https://www.mongodb.com/' },
+            { id: 8, name: 'Express.js', link: 'https://expressjs.com/' }
+        ]
+    })
+}
+</script>
 <style scoped>
 section {
-	@apply md:px-28 px-5 relative flex flex-col justify-center;
-	min-height: 80vh;
+    @apply md:px-28 px-5 relative flex flex-col justify-center;
+    min-height: 80vh;
 }
 
 h2 {
-	@apply text-crimson font-bold text-2xl mb-4;
-	@apply md:text-3xl;
+    @apply text-crimson font-bold text-2xl mb-4;
+    @apply md:text-3xl;
 }
 
 p {
-	@apply lg:w-2/3;
+    @apply lg:w-2/3;
 }
 
 .technologies {
-	@apply grid grid-cols-3 grid-rows-3 gap-3 lg:w-1/3 mt-3;
+    @apply grid grid-cols-3 grid-rows-3 gap-3 lg:w-1/3 mt-3;
 }
 .technology {
-	@apply bg-crimson  text-center font-bold p-2 rounded-sm cursor-pointer select-none;
-	@apply hover:bg-han-blue transition-colors duration-300;
+    @apply bg-crimson  text-center font-bold p-2 rounded-sm cursor-pointer select-none;
+    @apply hover:bg-han-blue transition-colors duration-300;
 }
 </style>
--- src/components/home/skills-section.vue
+++ src/components/home/skills-section.vue
@@ -1,64 +1,80 @@
 <template>
-	<section id="skills">
-		<!-- title -->
-		<h2>02. Skills</h2>
+    <section id="skills">
+        <!-- title -->
+        <h2>02. Skills</h2>
 
-		<!-- skills -->
-		<div class="skills">
-			<!-- programming languages -->
-			<div class="skills-list">
-				<div class="skills-list--title">Languages</div>
-				<div class="skill">00. Javascript</div>
-				<div class="skill">01. Typescript</div>
-				<div class="skill">02. Python</div>
-				<div class="skill">03. HTML</div>
-				<div class="skill">04. CSS</div>
-			</div>
-
-			<!-- tools -->
-			<div class="skills-list">
-				<div class="skills-list--title">Tools</div>
-				<div class="skill">00. Git + Github</div>
-				<div class="skill">01. Command Line</div>
-				<div class="skill">02. Linux</div>
-				<div class="skill">03. Insomnia</div>
-				<div class="skill">04. VScode</div>
-			</div>
-
-			<!-- frameworks -->
-			<div class="skills-list">
-				<div class="skills-list--title">Frameworks</div>
-				<div class="skill">00. Vue.js</div>
-				<div class="skill">01. React.js</div>
-				<div class="skill">02. Flutter</div>
-				<div class="skill">03. Express.js</div>
-				<div class="skill">04. Feathers.js</div>
-			</div>
-		</div>
-	</section>
+        <!-- skills -->
+        <div class="skills">
+            <div class="skills-list" v-for="skill in skills" :key="skill.id">
+                <div class="skills-list--title">{{ skill.title }}</div>
+                <div class="skill" v-for="_ in skill.skills" :key="_.id">{{ _.name }}</div>
+            </div>
+        </div>
+    </section>
 </template>
-
+<script>
+export default {
+    name: 'SkillsSection',
+    data: () => ({
+        skills: [
+            {
+                id: 0,
+                title: '00. Languages',
+                skills: [
+                    { id: 0, name: '00. Javascript' },
+                    { id: 1, name: '01. Typescript' },
+                    { id: 2, name: '02. Python' },
+                    { id: 3, name: '03. HTML' },
+                    { id: 4, name: '04. CSS' }
+                ]
+            },
+            {
+                id: 1,
+                title: '01. Tools',
+                skills: [
+                    { id: 0, name: '00. Git + Github' },
+                    { id: 1, name: '01. Command Line' },
+                    { id: 2, name: '02. Linux' },
+                    { id: 3, name: '03. Insomnia' },
+                    { id: 4, name: '04. VScode' }
+                ]
+            },
+            {
+                id: 2,
+                title: '02. Frameworks',
+                skills: [
+                    { id: 0, name: '00. Vue.js' },
+                    { id: 1, name: '01. React.js' },
+                    { id: 2, name: '02. Flutter' },
+                    { id: 3, name: '03. Express.js' },
+                    { id: 4, name: '04. Feathers.js' }
+                ]
+            }
+        ]
+    })
+}
+</script>
 <style scoped>
 section {
-	@apply md:px-28 px-5 py-5 relative flex flex-col justify-center font-bold;
-	min-height: 80vh;
+    @apply md:px-28 px-5 py-5 relative flex flex-col justify-center font-bold;
+    min-height: 80vh;
 }
 
 h2 {
-	@apply text-crimson font-bold text-2xl mb-4;
-	@apply md:text-3xl;
+    @apply text-crimson font-bold text-2xl mb-4;
+    @apply md:text-3xl;
 }
 .skills {
-	@apply flex gap-10 flex-col;
-	@apply lg:flex-row  lg:justify-center lg:items-center;
+    @apply flex gap-10 flex-col;
+    @apply lg:flex-row  lg:justify-center lg:items-center;
 }
 
 .skills-list--title {
-	@apply text-han-blue font-bold text-xl my-4 mb-2;
-	@apply md:text-3xl;
+    @apply text-han-blue font-bold text-xl my-4 mb-2;
+    @apply md:text-3xl;
 }
 
 .skill {
-	@apply text-lg m-5 mx-0;
+    @apply text-lg m-5 mx-0;
 }
 </style>
