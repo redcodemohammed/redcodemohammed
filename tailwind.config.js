4e77b8d45d3a7f28b7330ff706927b0c8ae2b865
Mohammed <redcodemohammed@gmail.com>
2022-05-19 19:11:52 +0300

    added jit mode to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,21 @@
 module.exports = {
-	purge: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-	darkMode: false, // or 'media' or 'class'
-	theme: {
-		extend: {
-			colors: {
-				'han-blue': '#4a65c2',
-				'eerie-black': '#171717',
-				'rich-black': '#121212',
-				onyx: '#444444',
-				crimson: '#DA0037',
-				cultured: '#EDEDED',
-			},
-		},
-	},
-	variants: {
-		extend: {},
-	},
-	plugins: [require('@tailwindcss/forms')],
+  purge: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class',
+  jit: true,
+  theme: {
+    extend: {
+      colors: {
+        'han-blue': '#4a65c2',
+        'eerie-black': '#171717',
+        'rich-black': '#121212',
+        onyx: '#444444',
+        crimson: '#DA0037',
+        cultured: '#EDEDED',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/forms')],
 }
